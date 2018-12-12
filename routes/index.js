4efcb9f526b1349eb386a12e3aaa87d45375d440
NatsLuffy29 <guillaume.mrt.29@gmail.com>
2018-12-12 11:57:08 +0100

    Guillaume  : mise en place armypage

--- routes/index.js
+++ routes/index.js
@@ -59,6 +59,15 @@ function(req, res){
   res.render('logged/storage');
 });
 
+router.get('/army',
+function(req, res){
+  res.locals.title = 'Army'
+  res.render('logged/army')
+})
+
+
+
+/* GAME INTERFACE */
 
 
 
